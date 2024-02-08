65a238461acb4b979ca2b5eaa34aaa74f756e9c7
atpursuit <140565742+atpursuit@users.noreply.github.com>
2024-02-07 21:42:00 -0500

    index.js

--- index.js
+++ index.js
@@ -24,13 +24,7 @@ function capitalizeFirstLetter(str) {
     // Split the string into an array of words
     let words = str.split(' ');
 
-    // Capitalize the first letter of each word
-    for (let i = 0; i < words.length; i++) {
-        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-    }
 
-    // Join the words back together into a single string
-    return words.join(' ');
 }
 
 // 2. Write a function that takes an array of lowercase words, return the words that have one or more 'e' but do not start with 'e'.
